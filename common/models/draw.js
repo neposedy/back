6eaea8b6342fd60327791567f3e014cca5509db6
mojzis <mojzisek@gmail.com>
2015-06-05 18:51:57 +0200

    new anon prepared

--- common/models/draw.js
+++ common/models/draw.js
@@ -1,3 +1,36 @@
 module.exports = function(Draw) {
 
+  var user = app.models.user;
+  Draw.get_new = function(user, level, cb) {
+    // move this to user. load the user somewhere
+    var userPlayed = [];
+    //Result.find(
+    //  {where: {playerId: user}},
+    //  function(err, results){
+    //    if(err) throw err;
+    //    if(results) {
+    //      // foreach result fill userPla
+    //    }
+    //  }
+    //);
+
+    Draw.findOne(
+      {where: {level: level}},
+      function(err, draw){
+        if (err) throw err;
+        cb(null, draw);
+    });
+  }
+
+  Draw.remoteMethod(
+    'get_new',
+    {
+      accepts: [
+        {arg: 'user', type: 'string'},
+        {arg: 'level', type: 'string'}
+      ],
+      returns: {arg: 'draw', root: true},
+      http: { verb: 'GET' }
+    }
+  );
 };
--- common/models/user.js
+++ common/models/user.js
@@ -1,3 +1,11 @@
 module.exports = function(User) {
-
+  User.new_anon = function(cb) {
+    cb(null, 1);
+  }
+  User.remoteMethod(
+    'new_anon',
+    {
+      returns: {arg: 'user', root: true}
+    }
+  );
 };
